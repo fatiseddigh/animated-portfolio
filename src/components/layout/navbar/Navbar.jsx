8f47b6bd38626dd88289b1a9ba5042a862762261
fatiseddigh <seddighfati@gmail.com>
2023-11-23 17:31:10 +0330

    feat:add motion to navbar

--- src/components/layout/navbar/Navbar.jsx
+++ src/components/layout/navbar/Navbar.jsx
@@ -1,15 +1,35 @@
+import { motion } from "framer-motion";
 import "./navbar.scss";
 const Navbar = () => {
   return (
     <div className="navbar">
       <div className="container">
-        <img src="/logos.svg" alt="" className="logo" />
-        <div className="socials">
+        <motion.img
+          initial={{ opacity: 0, x: 0.5 }}
+          animate={{ opacity: 1, x: 10 }}
+          transition={{
+            type: "smooth",
+            duration: 1,
+          }}
+          src="/logos.svg"
+          alt=""
+          className="logo"
+        />
+
+        <motion.div
+          initial={{ opacity: 0, scale: 0.1 }}
+          animate={{ opacity: 1, scale: 0.9 }}
+          transition={{
+            type: "smooth",
+            duration: 1,
+          }}
+          className="socials"
+        >
           <img src="/dribbble.png" alt="" />
           <img src="facebook.png" alt="" />
           <img src="instagram.png" alt="" />
           <img src="youtube.png" alt="" />
-        </div>
+        </motion.div>
       </div>
     </div>
   );
